f5969a01b98721b9218b6e69b7b14cab89c9f56d
Anonymous <153983726+anonymousforyou12@users.noreply.github.com>
2023-12-16 16:05:48 +0530

    Add files via upload

---
+++ webkiller clone/helplist/helpp.py
@@ -0,0 +1,155 @@
+import os
+from colorama import Fore
+import time
+import sys
+def Banner():
+
+    os.system("clear")
+    
+    print(Fore.YELLOW_EX+"""\n    
+    
+       ██╗░░██╗░█████╗░░█████╗░░░██╗██╗███████╗██████╗░
+       ██║░░██║██╔══██╗██╔══██╗░██╔╝██║██╔════╝██╔══██╗
+       ███████║███████║██║░░╚═╝██╔╝░██║█████╗░░██████╔╝
+       ██╔══██║██╔══██║██║░░██╗███████║██╔══╝░░██╔══██╗
+       ██║░░██║██║░░██║╚█████╔╝╚════██║███████╗██║░░██║
+       ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░░░░░░╚═╝╚══════╝╚═╝░░╚═╝
+ ====================================================================
+ **                  WebSite : xsecurity.in                        **
+ **                 Developers : Writer                            **
+ **               Team Members : rishabh,ashutosh sir           **
+ **                   Thank's : .::rishabh::.                       **
+ ====================================================================          
+          """)       
+# ₿
+def infolist1():
+    time.sleep(0.1)
+    print(Fore.RED+" ["+Fore.YELLOW+"$"+Fore.RED+"]"+Fore.CYAN+" Choose one of the options below \n")
+    time.sleep(0.1)
+    print(Fore.LIGHTYELLOW_EX+" [1] Information Gathering\n")
+    time.sleep(0.1)
+    print(Fore.RED+" [2] CMS Detection\n") 
+    time.sleep(0.1)
+    print(Fore.YELLOW+" [3] Developer :)\n")
+    time.sleep(0.1)
+    print(Fore.BLUE+" [4] Exit . . .\n")
+
+
+
+
+
+
+def infolist2():
+    time.sleep(0.1)
+    print(Fore.GREEN+"  [1]"+Fore.BLUE+" - Bypass Cloud Flare")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.2)
+
+    print(Fore.GREEN+"  [2]"+Fore.BLUE+" - Cms Detect")   
+    print(Fore.CYAN+"  **********************")
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [3]"+Fore.BLUE+" - Trace Toute")   
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [4]"+Fore.BLUE+" - Reverse IP")   
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [5]"+Fore.BLUE+" - Port Scan")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [6]"+Fore.BLUE+" - IP location Finder")
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [7]"+Fore.BLUE+" - Show HTTP Header")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [8]"+Fore.BLUE+" - Find Shared DNS")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [9]"+Fore.BLUE+" - Whois")    
+    print(Fore.CYAN+"  **********************")  
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [10]"+Fore.BLUE+" - DNS Lookup")   
+    print(Fore.CYAN+"  **********************")  
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [11]"+Fore.BLUE+"- Robots Scanner")    
+    print(Fore.CYAN+"  **********************")  
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [12]"+Fore.BLUE+" - Admin Page Finder")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [13]"+Fore.BLUE+" - Back To Menu")    
+    print(Fore.CYAN+"  **********************")
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [14]"+Fore.BLUE+" - Exit :) \n")   
+
+def infolist3():
+
+      
+      Banner()
+      time.sleep(0.1)
+      print (Fore.GREEN+" [*]"+Fore.BLUE+"  Develper :Rishabh:) \n")
+      time.sleep(0.1)
+      print (Fore.GREEN+" [*]"+Fore.GREEN+"  Thank's : .::Ashautosh sir::.\n")
+      time.sleep(0.1)
+      print (Fore.GREEN+" [*]"+Fore.MAGENTA+"  My Site : https://xsecurity.in/ :)\n")
+      time.sleep(0.1)
+      print (Fore.GREEN+" [*]"+Fore.CYAN+"  My Telegram ID @I_AM_HACKER_01s :]\n")
+      time.sleep(0.1)
+      try:
+
+            input(Fore.LIGHTRED_EX+" [*]  Back To Menu (Press Enter...) ")
+      except:
+            print("")
+            print("\n")
+            sys.exit()
+
+def infolist4():
+
+    Banner()
+    
+    print(Fore.GREEN+"  [1]"+Fore.BLUE+"- WordPress ")    
+    print(Fore.CYAN+"  **********************")  
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [2]"+Fore.BLUE+" - Drupal"+Fore.RED+" Coming Soon . . .")    
+    print(Fore.CYAN+"  **********************") 
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [3]"+Fore.BLUE+" - Joomla "+Fore.RED+" Coming Soon . . . ")    
+    print(Fore.CYAN+"  **********************")
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [4]"+Fore.BLUE+" - Back To Menu")   
+    
+    print(Fore.CYAN+"  **********************\n")  
+    time.sleep(0.1)
+
+
+def infowp():
+    Banner()
+    
+    print(Fore.GREEN+"  [1]"+Fore.BLUE+" - Get Plugins ")    
+    print(Fore.CYAN+"  **********************")  
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [2]"+Fore.BLUE+" - Get Username ")    
+    print(Fore.CYAN+"  **********************")
+    time.sleep(0.1)
+
+    print(Fore.GREEN+"  [3]"+Fore.BLUE+" - Back To Menu ")
+
+    print(Fore.CYAN+"  **********************\n")
+    time.sleep(0.1)
